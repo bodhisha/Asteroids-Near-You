d462aaeacdb04ad8de9c964c0a67dd7a5de5d13f
bodhisha <bodhisha@gmail.com>
2020-10-12 01:06:16 +0530

    add api key, fetch neo browse api

--- src/Components/ListAsteroids.jsx
+++ src/Components/ListAsteroids.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
 export default function ListAsteroids() {
-  const [asteroidData, setAsteroidData] = useState(null);
-
+  const [asteroidData, setAsteroidData] = useState([]);
   useEffect(() => {
+    console.log("render");
     async function fetchData() {
-      const res = await fetch();
+      const res = await fetch(
+        `https://api.nasa.gov/neo/rest/v1/neo/browse?page=0&size=10&api_key=${apiKey}`
+      );
+      const data = await res.json();
+      setAsteroidData(data.near_earth_objects);
     }
+    fetchData();
   }, []);
-  hxyeLdmWdqTn21JXbYijRxQbb36uEW9FAp8VcD7d;
-  return <div>haha</div>;
+  console.log(asteroidData.length);
+
+  return (
+    <>
+      {asteroidData.map((d) => {
+        return <div>name{d.name}</div>;
+      })}
+    </>
+  );
 }
