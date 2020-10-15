7582432bf29dfa53379aed63511aca91606aad68
bodhisha <bodhisha@gmail.com>
2020-10-15 10:20:37 +0530

    add to fav button

--- src/Components/ListAsteroids.jsx
+++ src/Components/ListAsteroids.jsx
@@ -18,8 +18,10 @@ export default function ListAsteroids() {
   return (
     <>
       <div className="max-w-5xl w-full mx-auto bg-blue-100 ">
-        <h3 className="text-3xl font-bold text-blue-800 m-2 ">Asteroids near Earth</h3>
-        <div className="flex flex-col "  >
+        <h3 className="text-3xl font-bold text-blue-800 m-2 ">
+          Asteroids near Earth
+        </h3>
+        <div className="flex flex-col ">
           {asteroidData.map((asteroid) => {
             return (
               <div className="bg-blue-300 p-2 m-2 rounded shadow ">
@@ -28,30 +30,60 @@ export default function ListAsteroids() {
                     {asteroid.name}
                   </p>
                   {asteroid.is_potentially_hazardous_asteroid ? (
-                    <p className="bg-blue-500 text-sm text-white font-bold p-1 block rounded-lg">Potentially Hazardous Asteroid</p>
-                  ) : <p className="bg-blue-500 text-sm text-white font-bold p-1 block rounded-lg">Non-Hazardous Asteroid</p>
-                  }
+                    <p className="bg-blue-500 text-sm text-white font-bold p-1 block rounded-lg">
+                      Potentially Hazardous Asteroid
+                    </p>
+                  ) : (
+                      <p className="bg-blue-500 text-sm text-white font-bold p-1 block rounded-lg">
+                        Non-Hazardous Asteroid
+                      </p>
+                    )}
                 </div>
                 <p className="text-sm text-blue-700 ">
                   Estimated Diameter of Asteroid:
-                  <span className="text-md text-blue-700 font-bold"> Min - {asteroid.estimated_diameter.kilometers.estimated_diameter_max} km , Max - {asteroid.estimated_diameter.kilometers.estimated_diameter_max}km</span>
+                  <span className="text-md text-blue-700 font-bold">
+                    Min -
+                    {
+                      asteroid.estimated_diameter.kilometers
+                        .estimated_diameter_max
+                    }
+                    km , Max -
+                    {
+                      asteroid.estimated_diameter.kilometers
+                        .estimated_diameter_max
+                    }
+                    km
+                  </span>
                 </p>
                 <p className="text-sm text-blue-700 ">
-                  Orbiting Body : <span className="text-md text-blue-700 font-bold"> {asteroid.close_approach_data.close_approach_date_full}</span>
+                  Orbiting Body :
+                  <span className="text-md text-blue-700 font-bold">
+                    {asteroid.close_approach_data.close_approach_date_full}
+                  </span>
                 </p>
                 <p className="text-sm text-blue-700 ">
                   Designation:
-                  <span className="text-md text-blue-700 font-bold">{asteroid.designation}</span>
+                  <span className="text-md text-blue-700 font-bold">
+                    {asteroid.designation}
+                  </span>
                 </p>
                 <p className="text-sm text-blue-700 ">
                   Absolute Magnitude:
-                  <span className="text-md text-blue-700 font-bold">{asteroid.absolute_magnitude_h}</span>
+                  <span className="text-md text-blue-700 font-bold">
+                    {asteroid.absolute_magnitude_h}
+                  </span>
                 </p>
-                <p className="text-sm text-blue-700 ">
-                  Last Observed on :
-                  <span className="text-md text-blue-700 font-bold"> {asteroid.orbital_data.last_observation_date}</span>
-                </p>
-
+                <div className="flex justify-between">
+                  <p className="text-sm text-blue-700 ">
+                    Last Observed on :
+                    <span className="text-md text-blue-700 font-bold">
+                      {asteroid.orbital_data.last_observation_date}
+                    </span>
+                  </p>
+                  <button className="bg-blue-500 text-sm text-white font-bold p-1 block rounded hover:bg-blue-400 hover:text-blue-700 ">
+                    Add to favourites
+                  </button>
+                </div>
               </div>
             );
           })}
@@ -60,4 +92,3 @@ export default function ListAsteroids() {
     </>
   );
 }
-
